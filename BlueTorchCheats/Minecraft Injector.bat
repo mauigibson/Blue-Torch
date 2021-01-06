191b5563397c3c97b5c992d8e7611dbbcfd13f20
mauigibson <mauigibson@users.noreply.github.com>
2021-01-05 17:28:00 -0800

    Add files via upload

---
+++ BlueTorchCheats/Minecraft Injector.bat
@@ -0,0 +1,66 @@
+@echo Off
+:start
+cls
+echo START THE GAME BEFORE PROCEEDING
+echo loading injector
+echo DO NOT CLOSE OR PRESS A BUTTON
+echo do not change any file names
+echo do not click on the EXE if this is open
+
+timeout 2 >nul
+Echo [#    ] 01%
+timeout 1 >nul
+cls
+echo loading injector
+echo DO NOT CLOSE OR PRESS A BUTTON
+echo do not change any file names
+echo do not click on the EXE if this is open
+Echo [##   ] 25%
+timeout 2.5 >nul
+cls
+echo loading injector
+echo DO NOT CLOSE OR PRESS A BUTTON
+echo do not change any file names
+echo do not click on the EXE if this is open
+Echo [###  ] 65%
+timeout 2 >nul
+cls
+echo loading injector
+echo DO NOT CLOSE OR PRESS A BUTTON
+echo do not change any file names
+echo do not click on the EXE if this is open
+Echo [#### ] 85%
+timeout 2 >nul
+cls
+echo loading injector
+echo DO NOT CLOSE OR PRESS A BUTTON
+echo do not change any file names
+echo do not click on the EXE if this is open
+Echo [#####] 100%
+cls
+echo loading injector
+echo DO NOT CLOSE OR PRESS A BUTTON
+echo do not change any file names
+echo do not click on the EXE if this is open
+color 4
+echo 1
+echo %username% /Blueflame.EXE
+echo loading BlueFlame
+echo checking game
+echo inserting .DLL
+echo coding reverse shell
+echo Checking for blue.cfg
+echo found blue.cfg
+echo loading required resources
+echo enabling multithread rendering
+
+cls
+echo Loading Injector
+echo do not click on the EXE if this is open
+timeout 2 >nul
+start C:\Users\%username%\Desktop\BlueTorchCheats\Hacks
+echo you have 15 seconds to launch the EXE
+timeout 15 >nul
+echo you have 5 seconds to launch the game if you did not already
+timeout 5 >nul
+
